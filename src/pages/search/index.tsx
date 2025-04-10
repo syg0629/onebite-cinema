e5abebbd1e26d6a1152d2ce28fb2d650ce276ced
syg0629 <syg0629@hanmail.net>
2025-04-10 21:51:19 +0900

    feat: 검색 페이지에 영화 목록 표시 기능 추가

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,24 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import movies from "@/mock/dummy.json";
+import MovieItem from "@/components/movie-item";
+import style from "./index.module.css";
+import { useRouter } from "next/router";
 
 export default function Page() {
   const router = useRouter();
-  const { q } = router.query;
-  return <h1>검색결과 : {q}</h1>;
+  const search = (router.query.q as string) || "";
+  const searchedMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className={style.container}>
+      {searchedMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
