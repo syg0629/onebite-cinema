8c359017c766edf769221f8b3b75b9f539c1f390
syg0629 <syg0629@hanmail.net>
2025-04-10 21:50:33 +0900

    feat: 영화 목록 표시를 위한 레이아웃 및 스타일 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
+import style from "./index.module.css";
+import movie from "@/mock/dummy.json";
+import MovieItem from "@/components/movie-item";
 
 export default function Home() {
-  return <h1>ONEBITE CINEMA</h1>;
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        <div className={style.recommand_container}>
+          {movie.slice(0, 3).map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.all_container}>
+          {movie.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
