3580fbb1bc6225b5eff29b0f04aa86b022b66f57
syg0629 <syg0629@hanmail.net>
2025-04-15 23:24:01 +0900

    feat: 서버 측에서 추천 영화와 모든 영화 데이터를 병렬로 가져오는 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,37 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
 import style from "./index.module.css";
-import movie from "@/mock/dummy.json";
 import MovieItem from "@/components/movie-item";
 import { InferGetServerSidePropsType } from "next";
 import fetchMovies from "@/lib/fetch-movies";
+import fetchRandomMovies from "@/lib/fetch-random-movies";
 
 export const getServerSideProps = async () => {
   // 사전 렌더링 과정에서 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수. 객체를 반환해야 함.
   // 서버 측에서 실행
-  const allMovies = await fetchMovies();
+
+  const [allMovies, recoMovies] = await Promise.all([
+    fetchMovies(),
+    fetchRandomMovies(),
+  ]); // 병렬로 요청
 
   return {
-    props: { allMovies },
+    props: { allMovies, recoMovies },
   };
 };
 
 export default function Home({
   allMovies,
+  recoMovies,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   //함수 반환값 타입을 추론하는 유틸리티
-  console.log(allMovies);
 
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.recommand_container}>
-          {movie.slice(0, 3).map((movie) => (
+          {recoMovies.slice(0, 3).map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
@@ -35,7 +39,7 @@ export default function Home({
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={style.all_container}>
-          {movie.map((movie) => (
+          {allMovies.map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
