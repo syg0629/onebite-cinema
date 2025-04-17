b0e1506334dea5cf7219f23e798e6b2ef17c569c
syg0629 <syg0629@hanmail.net>
2025-04-17 21:28:28 +0900

    feat: appRouter, pageRouter 디렉토리 파일 추가

---
+++ pageRouter/src/pages/movie/[id].tsx
@@ -0,0 +1,52 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import style from "./[id].module.css";
+import fetchOneMovie from "@/lib/fetch-one-movie";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const movie = await fetchOneMovie(Number(id));
+
+  return {
+    props: { movie },
+  };
+};
+
+export default function Page({
+  movie,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!movie) {
+    return "문제가 발생했습니다. 다시 시도해주세요.";
+  }
+
+  const {
+    id,
+    title,
+    releaseDate,
+    company,
+    genres,
+    subTitle,
+    description,
+    runtime,
+    posterImgUrl,
+  } = movie;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.poster_img_container}
+        style={{ backgroundImage: `url('${posterImgUrl}')` }}
+      >
+        <img src={posterImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.movie_details}>
+        {releaseDate} / {genres} / {runtime}
+      </div>
+      <div>{company}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
+}
