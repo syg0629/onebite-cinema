732b43a13d9e37b80be8514f4a8cf6bd0375f52e
syg0629 <syg0629@hanmail.net>
2025-04-24 23:56:06 +0900

    feat: API 호출을 위한 환경 변수 설정 및 오류 처리 추가, 불필요한 모의 데이터 삭제

--- appRouter/src/app/(with-searchbar)/page.tsx
+++ appRouter/src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,10 @@ import style from "./page.module.css";
 import { MovieData } from "@/types";
 
 async function Allmovies() {
-  const response = await fetch(`http://localhost:12345/movie`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SEVER_URL}/movie`,
+    { cache: "no-store" }
+  );
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
   const allmovies: MovieData[] = await response.json();
 
@@ -11,7 +14,10 @@ async function Allmovies() {
 }
 
 async function Recomovies() {
-  const response = await fetch(`http://localhost:12345/movie/random`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SEVER_URL}/movie/random`,
+    { next: { revalidate: 3 } }
+  );
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
   const recomovies: MovieData[] = await response.json();
 
--- appRouter/src/app/(with-searchbar)/search/page.tsx
+++ appRouter/src/app/(with-searchbar)/search/page.tsx
@@ -1,12 +1,18 @@
-import movies from "@/mock/movies.json";
 import MovieItem from "@/components/movie-item";
+import { MovieData } from "@/types";
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ q?: string }>;
+  searchParams: { q?: string };
 }) {
   const { q } = await searchParams;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SEVER_URL}/movie/search?q=${q}`
+  );
+  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  const movies: MovieData[] = await response.json();
 
   return (
     <div>
