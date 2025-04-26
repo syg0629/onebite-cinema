48157e6ba52f279f81d7f77909e0d19fdcb00f3e
syg0629 <syg0629@hanmail.net>
2025-04-26 23:59:57 +0900

    feat: Allmovies 함수에서 API 호출 캐시 설정을 'no-store'에서 'force-cache'로 변경

--- appRouter/src/app/(with-searchbar)/page.tsx
+++ appRouter/src/app/(with-searchbar)/page.tsx
@@ -5,7 +5,7 @@ import { MovieData } from "@/types";
 async function Allmovies() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SEVER_URL}/movie`,
-    { cache: "no-store" }
+    { cache: "force-cache" }
   );
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
   const allmovies: MovieData[] = await response.json();
