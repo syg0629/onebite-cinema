9b581709fb9e830c60a8e455747b99836f1885c4
syg0629 <syg0629@hanmail.net>
2025-04-30 23:50:06 +0900

    feat: 404 페이지를 위한 NotFound 컴포넌트 추가 및 generateStaticParams 함수 수정

--- appRouter/src/app/movie/[id]/page.tsx
+++ appRouter/src/app/movie/[id]/page.tsx
@@ -1,13 +1,16 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+// generateStaticParams로 만들지 않은 페이지는 다 404 에러 페이지로 이동
+// export const dynamicParams = "false";
 
 // 동적 경로를 사용하는 페이지를 정적 페이지로 만들고 싶다면,
 // `generateStaticParams`라는 약속된 함수를 사용해 빌드 타임에 어떤 URL 파라미터가 존재할지 직접 반환해야 함
 // URL 파라미터 값을 명시할 때에는 문자열 데이터로만 명시
 // generateStaticParams를 쓰는 순간, 그 페이지는 무조건 정적으로 생성, 컴포넌트 내부에 동적 데이터 패칭을 해도 무시
-function generateStaticParams(){
-  return [{id: "1" }, {id: "2" }, {id: "3" }];
+function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
-
 
 export default async function Page({
   params,
@@ -18,7 +21,12 @@ export default async function Page({
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SEVER_URL}/movie/${id}`
   );
-  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
+    return <div>오류가 발생했습니다...</div>;
+  }
   const movie = await response.json();
 
   const {
