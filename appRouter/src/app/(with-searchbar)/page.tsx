8bd1a7a05d6d15baaf70efb484c6cc12ee7fc74b
syg0629 <syg0629@hanmail.net>
2025-05-04 23:14:29 +0900

    fix: Searchbar 컴포넌트의 닫는 태그 수정 및 Allmovies와 Recomovies 함수에서 반환 형식 개선

--- appRouter/src/app/(with-searchbar)/page.tsx
+++ appRouter/src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,13 @@ import { delay } from "../util/delay";
 import { Suspense } from "react";
 import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
 
+// export const dynamic = "force-dynamic";
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정
+// 4. error : 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유 -> 빌드 오류)
+
 async function Allmovies() {
   await delay(1500);
   const response = await fetch(
@@ -14,7 +21,13 @@ async function Allmovies() {
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
   const allmovies: MovieData[] = await response.json();
 
-  return allmovies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+  return (
+    <div>
+      {allmovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 async function Recomovies() {
@@ -26,7 +39,13 @@ async function Recomovies() {
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
   const recomovies: MovieData[] = await response.json();
 
-  return recomovies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+  return (
+    <div>
+      {recomovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 export const dynamic = "force-dynamic";
